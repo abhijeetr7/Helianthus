a282b83e20865f2dd85482b26565ef6cc0ad9b10
Abhijeet <abhijit.sitafale@ravish.in>
2024-01-12 14:10:14 +0530

    Second Import Commit

---
+++ src/app/Homepage/homepage.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-homepage',
+    templateUrl: './homepage.html',
+    // styleUrls: ['./homepage.css']
+  })
+  
+  export class Homepage {
+    
+  }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Homepage } from './Homepage/homepage';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: Homepage },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
